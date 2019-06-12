fdd8951deb56b9c4925665151958f0b0fe3ab1b2
AbdlMalik <48346947+python-pro@users.noreply.github.com>
2019-06-12 02:33:48 -0500

    intact_logic
    The programs logic is pulled off. Still needs some work on printing out ordinal numbers.

--- primes.py
+++ primes.py
@@ -7,6 +7,11 @@
 # As getting your prompt, the program should return how many prime numbers for a given range and print them all out.
 
 # This project is not complete yet.
+""" Have to work on:
+---> On first three ordinal numbers, let it print out st, nd, rd respectively instead of th.
+---> Remove whitespaces left behind after looping through not prime numbers
+"""
+
 
 
 num=eval(input("What is your number: "))
@@ -15,14 +20,14 @@ rand_list=list()
 for a in range(2,num):
   for b in range(2, a):
     if a%b==0:
-      print("-----------------" )
+      print()
       break
 
-    else:
-      rand_list.append(a)
-      print()
-      print(f"The number {len(rand_list)} prime in {num} is -----> {a}")
-      break 
+  else:
+    rand_list.append(a)
+    print()
+    print(f"The  {len(rand_list)}th prime in {num} is -----> {a}")
+    continue
 
 print(f"There are {len(rand_list)} primes for a given range")
 print()
