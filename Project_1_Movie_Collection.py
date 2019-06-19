877330a4012f408c1bb20743edfcfae419e93458
AbdlMalik <48346947+python-pro@users.noreply.github.com>
2019-06-19 16:09:53 -0500

    changed

--- Project_1_Movie_Collection.py
+++ Project_1_Movie_Collection.py
@@ -4,6 +4,13 @@
 
 # M_S_A_W
 
+
+# You can limit the release years of movies
+# You can show the list of all movies then asking for movie then detail.
+# While finding for specific movie, input is case sensitive----You have to fix it later too.
+
+
+
 """
 - Enter "a" to add a movie, "l" to list all movies, "f" to find a movie, "q" to quit from the program
 
@@ -27,60 +34,85 @@ Tasks:
 """
 
 
-movies=list()
+
+movies = list()
 
 def menu():
-    user_input = input("Enter 'a' to add a movie, 'l' to list all movies, 'f' to find a movie, 'q' to quit from the program!  ")
+
+    user_input =input("""
+    \nTo add a movie press 'a',
+    \nTo see the list of  your movies press 's',
+    \nTo find a movie press 'f'
+    \nTo quit press 'q': \n""").lower()
 
     while user_input!='q':
 
-        if user_input == 'a':
+        if user_input=='a':
             add_movie()
 
-        elif user_input=='l':
-            list_movies()
+        elif user_input=='s':
+            show_movie()
 
         elif user_input=='f':
             find_movie()
 
         else:
-            print("Unkonwn command. Please try again. :)")
+            print("Unknown command. Please try again. :)\n")
 
-        user_input = input("\nEnter 'a' to add a movie, 'l' to list, 'f' to find , 'q' to quit  ")
+        user_input = input("\nEnter 'a' to add a movie, 's' to list, 'f' to find , 'q' to quit  \n")
 
 
 def add_movie():
     name=input("What is the name of the movie? ")
-    director=input("What is the director of the movie? ")
-    year=int(input("What is the release year of the movie? "))
+    director=input("Who is the director? ")
+    year=input("What the release year? ")
 
     movies.append({
-        'name':name,
+        'name': name,
         'director':director,
-        'year':year
+        'year': year
     })
 
-def list_movies():
-    for movie in movies:
-        list_movie_details(movie)
 
 
-def list_movie_details(movie):
+def show_movie(movie_list):
+    for mov in movie_list:
+        show_movie_details(mov)
+
+
+
+def show_movie_details(movie):
     print(f"Name: {movie['name']}")
     print(f"Director: {movie['director']}")
     print(f"Year: {movie['year']}")
 
+
 def find_movie():
-    find_by=input("What property of a movie are you looking for? ")
-    looking_for=input("What are you looking for ? ")
+    find_by=input("What property are you lookin for with? ")
+    lookin_for=input("What are you lookin for? ")
 
+    found_movies=find_by_attribut(movies, lookin_for, lambda x: x[find_by])
+
+    show_movie(found_movies)
+
+
+def find_by_attribut(items, expected, finder):
     found=list()
-
-    for movie in movies:
-        if movie['find_by']==looking_for:
-            found.append(movie)
+    for i in items:
+        if finder(i)==expected.lower():
+            found.append(i)
     return found
 
 
+    """movies.append({
+        'name': name,
+        'director':director,
+        'year': year
+    })"""
+
+
+
+
+
+
 menu()
-print(movies)
