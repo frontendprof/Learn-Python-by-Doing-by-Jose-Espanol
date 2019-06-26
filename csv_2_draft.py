01bce5e266c27f2d4eeb2ce0e974b42921531928
AbdlMalik <48346947+python-pro@users.noreply.github.com>
2019-06-26 12:13:26 -0500

    updated
    Ready to execute

--- csv_2_draft.py
+++ csv_2_draft.py
@@ -1,30 +1,37 @@
 # B_R_R
 # M_S_A_W
 
+import csv
 
 movies=[
-    {"name": "The Matrix", "director": "Wachowski"}
-    {"name": "Bourne Identity", "director": "Doug Liman"}
-    {"name": "Godfather", "director": "Francis Ford Coppola"}
-    {"name": "Shawshank Redemption", "director": "Frank Darabont"}
-    {"name": "Pulp Fiction", "director": "Quentin Tarantino"}
-    {"name": "Schindler's List", "director": "Steven Spielberg"}
+    {"name": "The Matrix", "director": "Wachowski"},
+    {"name": "Bourne Identity", "director": "Doug Liman"},
+    {"name": "Godfather", "director": "Francis Ford Coppola"},
+    {"name": "Shawshank Redemption", "director": "Frank Darabont"},
+    {"name": "Pulp Fiction", "director": "Quentin Tarantino"},
+    {"name": "Schindler's List", "director": "Steven Spielberg"},
     {"name": "Forrest Gump", "director": "Robert Zemeckis"}
-
 ]
 
 
 def write_to_file(output):
     with open("file.csv", 'w') as f:
-        f.write("name, director\n")
+        f.write("      NAME *\t\t\t       DIRECTOR\n")
+        f.write("=================*\t==================\n")
         for line in output:
-            f.write(f'{line["name"]}, {line['director']}\n')
+            f.write(f'{line["name"]}\t  *---> \t{line["director"]}\n')
 
 
 
 def read_from_file():
         with open('file.csv', 'r') as f:
             content=f.readlines()
-            for line in content[1:]:
-                columns=line.strip().split(',')
-                print(f'Name: {columns[0
+            for line in content:
+                columns=line.strip().split('*')
+                print(f'{columns[0]}\t{columns[1]}')
+
+
+
+
+write_to_file(movies)
+read_from_file()
