1b88b636da1650dbfb1f2787e67ad28ea2913e31
AbdlMalik <48346947+python-pro@users.noreply.github.com>
2019-06-27 21:56:31 -0500

    Create json_1.py

---
+++ json_1.py
@@ -0,0 +1,33 @@
+import json
+
+json_list = []
+
+"""
+We have data in csv_file.txt in csv format.
+We have to change it to dictionary data type in python first
+Then we have to move it to json_file and transfer it to json format file.
+"""
+
+"""
+Manchester United, Manchester, UK
+Real Madrid, Madrid, Spain
+Juventus, Turin, Italy
+Chelsea, London, UK
+"""
+
+
+csv_file = open('csv_file.txt', 'r')
+for line in csv_file.readlines():
+    club, city, country = line.strip().split(",")
+    data = {
+        "club": club,
+        "city": city,
+        "country": country
+    }
+    json_list.append(data)
+
+csv_file.close()
+
+json_file = open("json_file.txt", 'w')
+json_file.load(json_list, json_file)
+json_file.close()
