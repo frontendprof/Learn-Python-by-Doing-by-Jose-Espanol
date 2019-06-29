bf68c711feed0c1b1c26e839fa8c05b130d8b9c6
AbdlMalik <48346947+python-pro@users.noreply.github.com>
2019-06-29 17:05:40 -0500

    Create Proj_2_database_csv.py

---
+++ Proj_2_database_csv.py
@@ -0,0 +1,52 @@
+# B_R_R
+# M_S_A_W
+
+"""
+It stores and retrieves books from the CSV file.
+Format of CSV file:
+
+name,author,read\n
+name,author,read\n
+name,author,read\n
+name,author,read\n
+
+# comma would not be good idea to separate if comma is part of the name of the book
+"""
+
+books_file="books.txt"
+
+def create_book_table():
+    with open(books_file, 'w')as file:
+        pass
+
+def add_book(name, author):
+    with open(books_file, 'a') as file:
+        file.write(f'{name}, {author}, 0\n')
+
+
+def get_all_books():
+    with open(books_file, 'r')as file:
+        lines=[line.strip().split(',') for line in file.readlines()]
+
+    return[
+        {'name':line[0], 'author':line[1], 'read':line[2]}
+        for line in lines
+    ]
+
+def mark_book_as_read(name):
+    books=get_all_books()
+    for book in books:
+        if book['name']==name:
+            book['read']="1"
+    _save_all_books(books)
+
+def _save_all_books(books):
+    with open(books_file, 'w')as file:
+        for book in books:
+            file.write(f"{book['name']}, {book['author']},{book['read']}\n")
+
+def delete_book(name):
+    books = get_all_books()
+    books=[book for book in books if book['name']!=name]
+    _save_all_books(books)
+
