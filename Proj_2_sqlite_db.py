f335cd8cb9bd1def3d3a33785a7d229344f089b6
AbdlMalik <48346947+python-pro@users.noreply.github.com>
2019-07-01 19:24:18 -0500

    Create Proj_2_sqlite_db.py

---
+++ Proj_2_sqlite_db.py
@@ -0,0 +1,58 @@
+# B_R_R
+# M_S_A_W
+
+"""
+It stores and retrieves books from the JSON file.
+Format of JSON file:
+[
+    {
+        "name": 'Clean Code',
+        "author": "Robert",
+        'read': True
+    }
+]
+
+"""
+from .database_connection import DatabaseConnection
+
+
+books_file="books.json"
+
+def create_book_table():
+    with DatabaseConnection('data.db') as connection:
+
+        cursor=connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS books(name text primary key,author text,read integer)")
+
+
+def add_book(name, author):
+    with DatabaseConnection('data.db') as connection:
+        cursor=connection.cursor()
+        cursor.execute('INSERT INTO books VALUES(?,?,0)',(name,author))
+
+
+
+def get_all_books():
+    with DatabaseConnection('data.db') as connection:
+        cursor=connection.cursor()
+
+        cursor.execute("SELECT * FROM books")
+        books=[{"name":row[0],"author":row[1],"read":row[2]} for row in cursor.fetchall()]
+
+    return books
+
+
+
+def mark_book_as_read(name):
+    with DatabaseConnection('data.db') as connection:
+        cursor=connection.cursor()
+
+        cursor.execute('UPDATE books SET read=1 WHERE name=?',(name,))
+
+
+def delete_book(name):
+    with DatabaseConnection('data.db') as connection:
+        cursor=connection.cursor()
+
+        cursor.execute('DELETE from books WHERE name=?', (name,))
+
